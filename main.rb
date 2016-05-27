66a608f1bbf065a58f9cd0ebc9ba8a6cc44a2296
Jen <jen@reiher.ca>
2016-05-27 23:04:23 +0000

    working up until switching player

--- main.rb
+++ main.rb
@@ -1,6 +1,3 @@
-#@p1_score = 3
-#@p2_score = 3
-current_player = @players[1]
 
 @players = [
 	{
@@ -12,34 +9,55 @@ current_player = @players[1]
 		score: 3
 	}
 ]
-	
 
+#works
 def make_question(current_player)
 	rand_num = Random.new
-	@rand_num1 = rand_num.rand(10)
-	@rand_num2 = rand_num.rand(10)
-	"#{current_player} What does #{@rand_num1} plus #{@rand_num2} equal?"
+	rand_num1 = rand_num.rand(10)
+	rand_num2 = rand_num.rand(10)
+	@correct_answer = (rand_num1 + rand_num2)
+	puts "#{current_player[:name]}... What does #{rand_num1} plus #{rand_num2} equal?"
 end
 
-def print_score
-	"The score is currently:
-			#{@players[0][:name]} => #{@players[0][:score]}
-			#{@players[1][:name]} => #{@players[1][:score]}"
+#works
+def prompt_for_answer
+	@player_answer = gets.chomp.to_i
 end
 
-
-=begindef game_play(x) 
-	loop until someone loses all their lives
-		
-		print make_question
-		player_answer = gets.chomp
-		if player_answer != @rand_num1 + @rand_num2
-			@players[:score] -1
+#works
+def verify_answer(current_player)
+	puts "player answer is #{@player_answer}"
+	puts "the answer should be #{@correct_answer}"
+	puts "verifying score"
+	if @player_answer == @correct_answer
+			puts "correct! :)"
+		else
+			puts "wrong answer"
+			#does not work
+			current_player[:score]-1
 			print_score
-			else
-			@players[:score]
-		end
-	break loop
-end =end
+	end
+end
 
+# works
+def print_score
+	puts "The score is currently:
+	#{@players[0][:name]} => #{@players[0][:score]}
+	#{@players[1][:name]} => #{@players[1][:score]}"
+end
 
+# not called yet
+def change_player
+	@players.rotate!
+end
+
+# in progress
+def game_play 
+	current_player = @players[0]
+
+	make_question(current_player)
+	prompt_for_answer
+	verify_answer(current_player)
+end 
+
+game_play
